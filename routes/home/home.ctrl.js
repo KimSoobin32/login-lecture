9f2964514ae9329036866165b5161dc81f2ac152
KimSoobin32 <sbkim@exwaysoft.com>
2021-03-22 14:20:37 +0900

    깃 저장소 초기화

---
+++ routes/home/home.ctrl.js
@@ -0,0 +1,14 @@
+"use strict";
+const hello = (req, res) => {
+    //기능
+    res.render("home/index");
+};
+
+const login = (req, res) => {
+    res.render("home/login");
+ };
+
+ module.exports = {
+     hello,
+     login,
+ };
---
+++ routes/home/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+
+const ctrl = require("./home.ctrl");
+
+router.get("/", ctrl.hello);
+
+router.get("/login", ctrl.login);
+
+// 내보내기
+module.exports = router;
